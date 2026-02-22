2a242268ec4636be7882ac1e78cdfd6562001d04
tanishmundra-codes <tanishmundra2@gmail.com>
2026-02-22 12:16:59 +0530

    resolving bug

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,22 +1,22 @@
 import React from 'react'
-import { Header } from './layout'
+import { Header } from './components/Header'
 import Hero from './components/Hero'
 import { About } from './components/About'
 import { Features } from './components/Features'
 import { FAQ } from './components/Faq'
-import {Footer} from './components/Footer'
+import { Footer } from './components/Footer'
 import Pricing from './components/Pricing'
 
 function page() {
   return (
     <div>
-        <Header/>
-        <Hero/>
-        <About/>
-        <Features/>
-        <Pricing/>
-        <FAQ/>
-        <Footer/>
+      <Header />
+      <Hero />
+      <About />
+      <Features />
+      <Pricing />
+      <FAQ />
+      <Footer />
     </div>
   )
 }
